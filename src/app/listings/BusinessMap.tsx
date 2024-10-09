c0a43b1d9fb301709977d368f46d0d9b19058f79
Lucy Clarke <l.l.c.clarke@gmail.com>
2024-10-09 14:42:04 +0200

    add missing file

---
+++ src/app/listings/BusinessMap.tsx
@@ -0,0 +1,61 @@
+// BusinessMap.tsx
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Set up the icon for the marker
+const pawIcon = new L.Icon({
+  iconUrl: 'images/Dog_Paw_Print.png', // Replace with the path to your paw icon
+  iconSize: [30, 30], // Adjust the size as needed
+  iconAnchor: [15, 30],
+});
+
+type Business = {
+    business_id: number;
+    business_name: string;
+    address: string;
+    lat:number;
+    long:number;
+    description: string;
+    languages: [],
+    sizes: ('small' | 'medium' | 'large')[];
+    services: [];
+    company_logo: string;
+};
+
+type BusinessMapProps = {
+  businesses: Business[];
+};
+
+const BusinessMap: React.FC<BusinessMapProps> = ({ businesses }) => {
+  // You may need to calculate the center of the map based on the addresses
+  const center: [number, number] = [52.52437 , 13.41053]; // Default center (e.g., London)
+
+  
+  return (
+    <MapContainer center={center} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {businesses.map((business) => {
+        // Here you need to convert addresses to lat/lng
+        const lat = business.lat; 
+        const lng = business.long;
+
+        return (
+          <Marker key={business.business_id} position={[lat, lng]} icon={pawIcon}>
+            <Popup>
+              <strong>{business.business_name}</strong>
+              <br />
+              {business.address}
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
+  );
+};
+
+export default BusinessMap;
