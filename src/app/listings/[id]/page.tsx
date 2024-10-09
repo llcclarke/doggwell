0fd4d89309a108eff54364258468df929510701d
Lucy Clarke <l.l.c.clarke@gmail.com>
2024-10-09 09:38:21 +0200

    remove router

--- src/app/listings/[id]/page.tsx
+++ src/app/listings/[id]/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import { notFound } from 'next/navigation';
 
 type Listing = {
@@ -29,10 +28,6 @@ type ListingProps = {
 const ListingPage: React.FC<ListingProps> = async ({ params }) => {
   const listings = await fetchListings();
   const listing = listings.find((listing) => listing.business_id.toString() === params.id);
-  const router = useRouter();
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
   if (!listing) {
     return notFound();
   }
